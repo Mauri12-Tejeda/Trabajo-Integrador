a163e2365d9e13a2d910bcc5edea7662e8e659d2
mauri <mauri@DESKTOP-A7DLM0H>
2025-05-04 22:17:35 -0300

    Agregar archivos de proyecto.

---
+++ Trabajo Integrador/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+
+namespace Trabajo_integrador
+{
+
+    class Club
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+
+
+    class Deportes
+    {
+
+    }
+
+    class Personas
+    {
+        private string nombrePer, dniPer;
+        private int edadPer, categoriaPer, ultMesPago;
+        private double descuentoCuota;
+
+        //PROPIEDADES
+
+        public string NombrePer
+        {
+            set
+            {
+                nombrePer = value;
+            }
+            get
+            {
+                return nombrePer;
+            }
+        }
+
+        public string DniPer
+        {
+            set
+            {
+                dniPer = value;
+            }
+            get
+            {
+                return dniPer;
+            }
+
+        }
+
+        public int EdadPer
+        {
+            set
+            {
+                edadPer = value;
+            }
+            get
+            {
+                return edadPer;
+            }
+        }
+
+        public int CategoriaPer 
+        {
+            set
+            {
+                categoriaPer = value;
+            }
+            get
+            {
+                return categoriaPer;
+            }
+
+        }
+
+        public int UltMesPago
+        {
+            set
+            {
+                ultMesPago = value;
+            }
+            get
+            {
+                return ultMesPago;
+            }
+        }
+
+        public double DecuentoCuota
+        {
+            get
+            {
+                return descuentoCuota;
+            }
+        }
+    }
+}
